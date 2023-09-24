60646bf39c8c02e59fa4c5a4568ff18c694c8ada
anuj-rane0 <anujrane7614@gmail.com>
2023-09-24 12:25:46 +0530

    Hello Anuj C sharp is shit

---
+++ Hello World/EventsAndDelegates/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class Audio
+    {
+        public string Title{ get; set; }
+    }
+    public class AudioEncoder
+    {
+        public delegate void AudioEncoderEventHandler(object source, EventArgs args);
+
+        public event AudioEncoderEventHandler AudioEncoded;
+
+        public void Encode(Audio audio)
+        {
+            Console.WriteLine("Encoding Audio");
+
+            Thread.Sleep(6000);
+
+            OnAudioEncoded(); git config --global user.email "anujrane7614@gmail.com"
+
+
+
+        }
+
+        protected virtual void OnAudioEncoded(object source, EventArgs args) 
+        {
+        
+            AudioEncoded?.Invoke(this, EventArgs.Empty);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var audio = new Audio() { Title = "title1"};
+
+            var audioEncoder = new AudioEncoder();
+
+            var phoneService = new PhoneService();
+
+            audioEncoder.AudioEncoded += phoneService.OnAudioEncoded;
+
+            audioEncoder.Encode(audio);
+        }
+    }
+
+    public class PhoneService
+    {
+        public void OnAudioEncoded(object source, EventArgs args) 
+        {
+            Console.WriteLine("Phone Service");
+        }
+    }
+}
---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 11, 5, 1, 5 };
+            Console.WriteLine("Hello World");
+            int maxNum = 0;
+            int partition = 0;
+
+
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                for(int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        maxNum = array[i];
+                    }
+                    else
+                    {
+                        maxNum = array[j];
+                    }
+                }
+            }
+
+            Console.WriteLine(maxNum);
+        }
+    }
+}
